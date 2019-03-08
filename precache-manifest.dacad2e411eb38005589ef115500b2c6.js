bdd112d930f60d41f4f6962be92b3a49e71458d5
Guilherme Sousa <guilherme.lemos.sousa@everis.com>
2019-03-08 01:25:29 +0000

    Updates

---
+++ precache-manifest.dacad2e411eb38005589ef115500b2c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6be824305f173ac10a32",
+    "url": "/cartrack-challenge/static/js/runtime~main.6be82430.js"
+  },
+  {
+    "revision": "e1166a298df86fa47b62",
+    "url": "/cartrack-challenge/static/js/main.e1166a29.chunk.js"
+  },
+  {
+    "revision": "de5a392ed10c19514cc6",
+    "url": "/cartrack-challenge/static/js/2.de5a392e.chunk.js"
+  },
+  {
+    "revision": "e1166a298df86fa47b62",
+    "url": "/cartrack-challenge/static/css/main.62c170cc.chunk.css"
+  },
+  {
+    "revision": "de5a392ed10c19514cc6",
+    "url": "/cartrack-challenge/static/css/2.8afcf6de.chunk.css"
+  },
+  {
+    "revision": "e8e7658f0b6b1103aef8764a32e07ff1",
+    "url": "/cartrack-challenge/index.html"
+  }
+];
